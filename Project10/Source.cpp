b5060d4af2f5b65779e09a377ddca32073ed1751
Memme_xu14 <Memme_xu14@STHQ0124-16>
2022-11-11 19:59:10 +0400

    Add project files.

---
+++ Project10/Source.cpp
@@ -0,0 +1,71 @@
+﻿
+
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+//
+//Single inheritance
+//Əgər tək bir sinif bir əsas sinifdən yaranırsa, o, tək miras adlanır .
+class Water {
+
+};//base class;
+
+
+class Sirab : public Water {
+
+};//derived class;
+
+
+
+
+
+//Multi level inheritance
+//Əgər sinif başqa bir törəmə sinifdən yaranıbsa, ona çoxsəviyyəli miras deyilir.
+class Water {};//base class;
+
+
+class Naxcivan : public Water {
+
+};//derived class;
+
+class Badamli : public Naxcivan {
+
+};//derived class;
+
+
+
+
+//Multiple inheritance
+//Əgər bir sinif iki və ya daha çox əsas sinifdən yaranırsa, ona çoxlu irsiyyət deyilir.
+class Water {};//base class;
+
+class Naxcivan {};//base class;
+
+
+class Sirab : public Water, public Naxcivan {
+
+};//derived class;
+
+
+
+
+//Hierarchy
+//Ümumi əsas sinifdən bir neçə sinif yarandıqda buna iyerarxik irsiyyət deyilir .
+class Water {};//base class;
+
+class Sirab : public Water {};//derived class;
+
+class Badamli : public Water {};//derived class;
+
+class Daridag : public Water {};//derived class;
+
+
+
+
+int main() {
+
+
+}
